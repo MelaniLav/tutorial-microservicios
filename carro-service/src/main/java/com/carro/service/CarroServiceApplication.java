702f3370b6becaaa17e9d74629da975d68d2c631
melani yulli laveriano porroa <melani.laveriano@unmsm.edu.pe>
2024-03-24 16:03:03 -0500

    eureka

---
+++ carro-service/src/main/java/com/carro/service/CarroServiceApplication.java
@@ -0,0 +1,15 @@
+package com.carro.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CarroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarroServiceApplication.class, args);
+    }
+
+}
